bb2aa0c1944797d1f128ff4258c058537e44b732
ammarkandel <merom8703@gmail.com>
2020-12-28 23:57:25 +0200

    modify for linter

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,16 @@
 def bubble_sort(array1)
-  arrLen = array1.length() - 2
-  (arrLen).downto(0).each {
-     |i|
-      (0).upto(arrLen).each {
-        |j|
+  arr_len = array1.length - 2
+  arr_len.downto(0).each { |i|
+      0.upto(arr_len).each {
+      |j|
         if array1[j] > array1[j+1]
            swap = array1[j]
            array1[j] = array1[j+1]
            array1[j+1] = swap
          end
+        }
       }
-    }
-    puts array1
+  puts array1
 end
 
-bubble_sort([25,2,1,15,3,8])
+bubble_sort([25, 2, 1, 15, 3, 8])
