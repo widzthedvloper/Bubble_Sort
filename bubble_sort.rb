c5d6491cfdd9e2b4b27cfed5b841602d76596617
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2020-12-29 11:32:14 -0400

    add second method with yield and block

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,4 +13,21 @@ def bubble_sort(integers)
   integers
 end
 
-puts bubble_sort([25, 2, 1, 15, 3, 8])
+def bubble_sort_by(integers)
+  size = integers.length - 2
+  size.downto(0).each do
+    0.upto(size).each do |j|
+     next if yield(integers[j], integers[j + 1]) < 0
+
+      swap = integers[j]
+      integers[j] = integers[j + 1]
+      integers[j + 1] = swap
+    end
+  end
+
+  integers
+end
+
+puts (bubble_sort_by([25, 25, 1, 25, 3, 8]) do |left, right|
+  left - right
+end)
