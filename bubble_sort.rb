7a12519f18f379a83b239be72eca86a570bed303
ammarkandel <merom8703@gmail.com>
2020-12-29 02:06:14 +0200

    modify linter

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,15 +1,14 @@
 def bubble_sort(array1)
   arr_len = array1.length - 2
   arr_len.downto(0).each do
-  0.upto(arr_len).each do |j|
-    next if array1[j] < array1[j + 1]
+    0.upto(arr_len).each do |j|
+      next if array1[j] < array1[j + 1]
+
       swap = array1[j]
       array1[j] = array1[j + 1]
       array1[j + 1] = swap
+    end
   end
-  end
-
-puts array1
+  puts array1
 end
-
 bubble_sort([25, 2, 1, 15, 3, 8])
